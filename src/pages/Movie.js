6dd754958b77d18252271dd60f3cbc4e0768a149
John <jaricaya@gmail.com>
2024-07-24 04:41:38 -0700

    solved warning

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,40 +1,41 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import { useParams } from "react-router-dom";
 
 function Movie() {
-  const [movie,setMovie] = useState(null);
-  const params = useParams();
-  const movieId = params.id
-  
-useEffect(() => {
-  fetch(`http://localhost:4000/movies/${movieId}`)
-  .then(res => res.json())
-  .then(data => setMovie(data))
-  .catch(error => console.log(`Failed to load movie due to ${error}`))
-}, [movieId])
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
 
-if(!movie){
-  return <h1>Loading...</h1>
-}
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((res) => res.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.log(`Failed to load movie due to ${error}`));
+  }, [id]);
 
-const {title, time, genres} = movie
- {movie.genres.map(genre => <span key={genre}> {genre} </span>)}
+  if (!movie) {
+    return <h1>Loading...</h1>;
+  }
+
+  const { title, time, genres } = movie;
 
   return (
     <>
       <header>
-        {/* What component should go here? */}
-        < NavBar />
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
-        <h1> {title} </h1>
-        <p> Time:{time} </p>
-        {genres.map(genre => <span key={genre}> {genre} </span>)}
+        <h1>{title}</h1>
+        <p>Time: {time}</p>
+        <div>
+          Genres:
+          {genres.map((genre) => (
+            <span key={genre}> {genre} </span>
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Movie;
